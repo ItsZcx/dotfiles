fa781c9cd0a9ff50702fc1f8b8a41d4541dbe7e7
zcx <joanpaumeridaruiz@gmail.com>
2026-09-08 11:11:48 +0200

    Remove zsh-autosuggestions and zsh-syntax-highlighting plugins

--- run_once_before_setup-zsh-10.sh
+++ run_once_before_setup-zsh-10.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
-
 # Install Oh My Zsh non-interactively
 if [ ! -d "${HOME}/.oh-my-zsh" ]; then
   echo "==> Installing Oh My Zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
-
-# Install zsh-autosuggestions
-if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
-fi
-
-# Install zsh-syntax-highlighting
-if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
 fi
